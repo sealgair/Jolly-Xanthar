8fc3b639aff1b4bf73b619ca2f55762c153f8674
Chase Caster <sealgair@gmail.com>
2015-10-26 22:08:51 -0700

    palette from file

--- mobs/human.lua
+++ mobs/human.lua
@@ -4,20 +4,27 @@ require 'weapons.bolter'
 Human = Mob:extend('Human')
 
 function Human:init(coord)
-  local hue1 = math.random(255)
-  local hue2 = math.random(255)
+  local palette = love.graphics.newImage('assets/palette.bmp'):getData()
+  local choices = palette:getHeight()
+
+  local toColors = {}
+  local seen = {}
+  for i = 1,3 do
+    local c
+    repeat c = math.random(0, choices) until seen[c] == nil
+    seen[c] = true
+    table.insert(toColors, {palette:getPixel(0, c)})
+    table.insert(toColors, {palette:getPixel(1, c)})
+  end
 
   self.shader = palletSwapShader({
     {255, 0, 0},
     {128, 0, 0},
+    {0, 255, 0},
+    {0, 128, 0},
     {0, 0, 255},
     {0, 0, 128},
-  }, {
-    HSVtoRGB(hue1, 188, 255),
-    HSVtoRGB(hue1, 188, 128),
-    HSVtoRGB(hue2, 188, 255),
-    HSVtoRGB(hue2, 188, 128),
-  })
+  }, toColors)
 
   Human.super.init(self, {
     x = coord.x,
